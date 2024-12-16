cedcba02633a7dd36175dc5b0c750159d0459a25
Limonchik-D <DavLimon4@yandex.com>
2024-12-16 21:24:00 +0200

    Add files via upload

---
+++ Task 4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    internal class Program
+    {/// <summary>
+     /// Задача 4. Сортировка массива по возрастанию.
+     /// </summary>
+        static void Main(string[] args)
+        {
+            int[] array = { 9, 10, 98, 5, 6, 99, 36, 100 };
+            Array.Sort(array);
+            foreach (var all in array) 
+            {
+                Console.Write(all + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
