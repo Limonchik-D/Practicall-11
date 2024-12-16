21ac30cc14010b2519911566ebcb7e49f9aae92b
Limonchik-D <DavLimon4@yandex.com>
2024-12-16 21:22:24 +0200

    Add files via upload

---
+++ Практическая №11/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практическая__11
+{
+    internal class Program
+    {/// <summary>
+     /// Задача 1. Определить количество элементов одномерного массива.
+     /// </summary>
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine($"Кол-во элементов массива: {array.Length}");
+            Console.ReadKey();
+        }
+    }
+}
