745920daa39c7d41689f685a4ab990ff8fc04673
Limonchik-D <DavLimon4@yandex.com>
2024-12-16 21:23:28 +0200

    Add files via upload

---
+++ Task 3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    internal class Program
+    {/// <summary>
+     /// Задача 3. Найти максимальное и минимальное значение в массиве.
+     /// </summary>
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3 };
+
+            int max = array.Max();
+            int min = array.Min();
+            Console.Write($"Минимальныйэлемент: {min} и максимальный элемент: {max}");
+            Console.ReadKey();
+        }
+    }
+}
