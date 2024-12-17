91cf5f8ef2db085242dc389d82152e9ba0b87988
Limonchik-D <DavLimon4@yandex.com>
2024-12-17 20:54:08 +0200

    Add files via upload

---
+++ Task 7/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_7
+{
+    internal class Program
+    {/// <summary>  
+    /// Поиск элемента по условию
+    /// </summary>
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5 };
+
+            int result = array.FirstOrDefault(x => x > 4);
+
+            if (result != 0)
+            {
+                Console.WriteLine($"Элемент больше четырёх: {result}");
+            }
+            else 
+            {
+                Console.WriteLine("Ошибка");
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
