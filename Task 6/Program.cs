876e11fa90c0ac1f8c48e737dc11001ee9f0b19f
Limonchik-D <DavLimon4@yandex.com>
2024-12-17 20:53:35 +0200

    Add files via upload

---
+++ Task 6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_6
+{
+    internal class Program
+    {   /// <summary>
+        /// Подсчёт кол-во вхождений определённого числа в массив 
+        /// </summary>
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8 };
+            int number = 4;
+            int count = array.Count(x => x == number);
+            Console.WriteLine($"Кол-во вхождений в число {number}, равно {count} раз");
+
+            Console.ReadKey();
+        }
+    }
+}
