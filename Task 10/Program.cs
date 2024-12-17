c27e93971db3dab7c78d9b379cc3e26c07f8ab60
Limonchik-D <DavLimon4@yandex.com>
2024-12-17 20:56:23 +0200

    Add files via upload

---
+++ Task 10/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_10
+{
+    internal class Program
+    {/// <summary>
+    /// Удаление всех чётных элементов из массива
+    /// </summary>
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Console.WriteLine($"Исходный массив: {string.Join(", ", array)}");
+
+            int[] result = array.Where(x => x % 2 != 0).ToArray();
+            Console.WriteLine($"Результат: {string.Join(", ", result)}");
+
+            Console.ReadLine();
+        }
+    }
+}
