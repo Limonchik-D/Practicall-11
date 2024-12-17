8ef1eb75227fc85130d4a6eae5c487fe6560adfc
Limonchik-D <DavLimon4@yandex.com>
2024-12-17 20:52:48 +0200

    Add files via upload

---
+++ Task 5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5
+{
+    internal class Program
+    {/// <summary>
+    /// Реверс массива
+    /// </summary>
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5 };
+            Array.Reverse(array);
+            for (int i = 0; i < array.Length; i++) 
+            {
+                Console.WriteLine(array[i]);
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
