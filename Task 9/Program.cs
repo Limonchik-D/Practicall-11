9a2ec1bda4eccab84c700d3938f6b4d213373e4d
Limonchik-D <DavLimon4@yandex.com>
2024-12-17 20:55:46 +0200

    Add files via upload

---
+++ Task 9/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_9
+{
+    internal class Program
+    {/// <summary>
+    /// Поиск индекса элемента
+    /// </summary>
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5 };
+
+            int number = 4;
+            int result = Array.IndexOf(array, number);
+            Console.WriteLine($"Индекс элеменат {number}: {result}");
+
+            Console.ReadKey(); 
+        }
+    }
+}
